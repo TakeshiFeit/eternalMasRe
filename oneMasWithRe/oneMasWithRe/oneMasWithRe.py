a508f09d9f8f3ddeb4e4d7345d6a5ce67ca1252c
TakeshiFeit <feit.and@mail.ru>
2024-11-08 12:47:21 +0400

    Add files via upload

--- oneMasWithRe/oneMasWithRe/oneMasWithRe.py
+++ oneMasWithRe/oneMasWithRe/oneMasWithRe.py
@@ -27,13 +27,18 @@ while txt != []:
     for i in range(len(txt)):
         cond = False
         replaced = ""
+        if int(txt[i], 16) > 0:
+            txt[i] = "+"+ txt[i]
+            sign = ""
+        else:
+            sign = "-"
         if Max < int(txt[i],16):
             Max = int(txt[i],16)
             cond = True; index = i
-        txt2 = re.sub("(.)", r'\1 ', txt[i])
-        txt2 = [int(txt2[x], 16) for x in range(0, len(txt2), 2)]
+        txt2 = re.sub("(.)", r'\1 ', str(txt[i]))
+        txt2 = [int(txt2[x], 16) for x in range(2, len(txt2), 2)]
         replaced += num.sub(lambda x: num2some[int(x.group())], str(txt2))
         txt[i] = replaced
-        print (txt[i]) if (cond == True) else()
+        print (sign + txt[index]) if (cond == True) else()
     txt = file.readline().split()
 
